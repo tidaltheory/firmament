ef52411f4d8ff2763926e9df41438722e47b858d
Jeff <rustyangel@gmail.com>
2021-06-10 09:31:40 -0600

    Chore: Add missing eslint package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
     extends: [
-        '@zazen',
         '@zazen/eslint-config/vue',
-        '@vue/typescript/recommended',
-        '@vue/prettier',
-        '@vue/prettier/@typescript-eslint',
+        // '@vue/typescript/recommended',
+        // '@vue/prettier',
+        // '@vue/prettier/@typescript-eslint',
     ],
+    parserOptions: {
+        project: null,
+    },
     rules: {
         'prefer-const': 'off',
         'vue/html-indent': 'off',
